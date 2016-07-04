86f492ec69947e088704a4f086258c95c21004fe
Mario Canul <mcanul@hawaii.edu>
2016-07-04 13:53:56 -1000

    Create a WebView for EddystoneURL

--- app/src/main/java/edu/hawaii/maui/index/educationalbeacons/EddystoneURL.java
+++ app/src/main/java/edu/hawaii/maui/index/educationalbeacons/EddystoneURL.java
@@ -2,6 +2,7 @@ package edu.hawaii.maui.index.educationalbeacons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class EddystoneURL extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class EddystoneURL extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eddystone_url);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://beaconcafeteria.wordpress.com");
     }
 }
