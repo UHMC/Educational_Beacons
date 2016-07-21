14d38c30e0a365113d24a00dd6b62cf571fddd79
Saxon Knight <knight7@hawaii.edu>
2016-07-20 14:01:12 -1000

    Add detection code to EddystoneEID.java

--- app/src/main/java/edu/hawaii/maui/index/educationalbeacons/EddystoneEID.java
+++ app/src/main/java/edu/hawaii/maui/index/educationalbeacons/EddystoneEID.java
@@ -1,13 +1,65 @@
 package edu.hawaii.maui.index.educationalbeacons;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class EddystoneEID extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class EddystoneEID extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+    private static final String TAG="EddystoneEID";
+    private BeaconManager mBeaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eddystone_eid);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect Eddystone-EID
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_EID_LAYOUT)); // "s:0-1=feaa,m:2-2=30,p:3-3:-41,i:4-11"
+        mBeaconManager.bind(this);
+    }
+
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mBeaconManager.setRangeNotifier(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        for (Beacon beacon : beacons) {
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x30) {
+                // This is a Eddystone-EID frame
+                Identifier ephemeralId = beacon.getId1();
+                Log.d(TAG, "I see a beacon transmitting ephemeral id: " + ephemeralId +
+                        " approximately " + beacon.getDistance() + " meters away.");
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
     }
 }
