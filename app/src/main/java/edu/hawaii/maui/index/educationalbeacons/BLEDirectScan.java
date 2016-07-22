423add30772203e851678ac585a06ade2fe2bc3a
Saxon Knight <knight7@hawaii.edu>
2016-07-22 10:26:17 -1000

    Grab services and add enablement in BLEDirectScan

--- app/src/main/java/edu/hawaii/maui/index/educationalbeacons/BLEDirectScan.java
+++ app/src/main/java/edu/hawaii/maui/index/educationalbeacons/BLEDirectScan.java
@@ -1,13 +1,33 @@
 package edu.hawaii.maui.index.educationalbeacons;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class BLEDirectScan extends AppCompatActivity {
+    private final static int REQUEST_ENABLE_BT = 1;
+    private final static String TAG = "BLEDirectScan";
+    private final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bledirect_scan);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled())
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) Log.d(TAG, "Bluetooth enabled.");
+            else Log.d(TAG, "Bluetooth not enabled!");
+        }
     }
 }
