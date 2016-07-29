4eb253a04f3e05ebc6bb145a736b455545c400ce
Saxon Knight <knight7@hawaii.edu>
2016-07-28 16:25:09 -1000

    Clean up EddystoneEID
    - Removes advertize + button
    - Uses ephemeralId instead of beacon.getId1()
    - Removes unused imports
    - Cleans up debug lines
    - Reformats code with Android Studio's default

--- app/src/main/java/edu/hawaii/maui/index/educationalbeacons/EddystoneEID.java
+++ app/src/main/java/edu/hawaii/maui/index/educationalbeacons/EddystoneEID.java
@@ -1,32 +1,25 @@
 package edu.hawaii.maui.index.educationalbeacons;
 
-import android.bluetooth.le.AdvertiseCallback;
-import android.bluetooth.le.AdvertiseSettings;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.BeaconTransmitter;
 import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.utils.eddystoneeid.EidResolver;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class EddystoneEID extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
     private static final String TAG = "EddystoneEID";
-    private static final String DEBUG_TAG = "EddystoneEID_Debug";
     private static final boolean DEBUG = true;
     private BeaconManager mBeaconManager;
     private static final String GOOGLE_API_KEY = "AIzaSyDPC7KjBKOUlbrEFvptg3qtYBk3_CJgULU";
@@ -40,7 +33,7 @@ public class EddystoneEID extends AppCompatActivity implements BeaconConsumer, R
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eddystone_eid);
-        if (DEBUG) Log.d(DEBUG_TAG, "onCreate");
+        if (DEBUG) Log.d(TAG, "onCreate");
     }
 
     @Override
@@ -51,7 +44,7 @@ public class EddystoneEID extends AppCompatActivity implements BeaconConsumer, R
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout(BeaconParser.EDDYSTONE_EID_LAYOUT)); // "s:0-1=feaa,m:2-2=30,p:3-3:-41,i:4-11"
         mBeaconManager.bind(this);
-        if (DEBUG) Log.d(DEBUG_TAG, "onResume");
+        if (DEBUG) Log.d(TAG, "onResume");
     }
 
     public void onBeaconServiceConnect() {
@@ -61,35 +54,34 @@ public class EddystoneEID extends AppCompatActivity implements BeaconConsumer, R
             mBeaconManager.startRangingBeaconsInRegion(region);
         } catch (RemoteException e) {
             e.printStackTrace();
-            if (DEBUG) Log.d(DEBUG_TAG, "Error");
+            if (DEBUG) Log.d(TAG, "Error");
         }
         mBeaconManager.setRangeNotifier(this);
-        if (DEBUG) Log.d(DEBUG_TAG, "onBeaconServiceConnect");
+        if (DEBUG) Log.d(TAG, "onBeaconServiceConnect");
     }
 
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-        if (DEBUG) Log.d(DEBUG_TAG, "didRangeBeaconsInRegion");
+        if (DEBUG) Log.d(TAG, "didRangeBeaconsInRegion");
         for (Beacon beacon : beacons) {
-            if (DEBUG) Log.d(DEBUG_TAG, "Another beacon");
+            if (DEBUG) Log.d(TAG, "Another beacon");
             if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x30) {
-                if (DEBUG) Log.d(DEBUG_TAG, "Matches EID type");
+                if (DEBUG) Log.d(TAG, "Matches EID type");
                 // This is a Eddystone-EID frame
                 Identifier ephemeralId = beacon.getId1();
-                Log.d(TAG, "I see a beacon transmitting ephemeral id: " + ephemeralId +
+                if (DEBUG) Log.d(TAG, "I see a beacon transmitting ephemeral id: " + ephemeralId +
                         " approximately " + beacon.getDistance() + " meters away.");
-                Log.d(DEBUG_TAG, "B64_OUT: [" + Base64.encodeToString(beacon.getId1().toByteArray(), Base64.NO_WRAP) + "]");
-                if (resolver.getResolvedIdentifierString(beacon.getId1()) != null) {
-                    attachmentData = Base64.decode(resolver.getResolvedIdentifierString(beacon.getId1()).getBytes(),Base64.NO_WRAP);
+                if (resolver.getResolvedIdentifierString(ephemeralId) != null) {
+                    attachmentData = Base64.decode(resolver.getResolvedIdentifierString(ephemeralId).getBytes(), Base64.NO_WRAP);
                     try {
-                        data = new String(attachmentData,"UTF-8");
+                        data = new String(attachmentData, "UTF-8");
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
-                    Log.d(DEBUG_TAG, "Resolved identifier for: " + beacon.getId1() + " is " + data);
-
+                    if (DEBUG)
+                        Log.d(TAG, "Resolved identifier for: " + ephemeralId + " is " + data);
                 }
-                if (DEBUG) Log.d(DEBUG_TAG, "Logged some range info");
+                if (DEBUG) Log.d(TAG, "Logged some range info");
             }
         }
     }
@@ -98,31 +90,6 @@ public class EddystoneEID extends AppCompatActivity implements BeaconConsumer, R
     public void onPause() {
         super.onPause();
         mBeaconManager.unbind(this);
-        if (DEBUG) Log.d(DEBUG_TAG, "onPause");
-    }
-
-    public void advertise(View view) {
-        if (DEBUG) Log.d(DEBUG_TAG, "Advertise");
-        Beacon beacon = new Beacon.Builder()
-                .setId1("0x0001020304050607") // Ephemeral Identifier
-                .setManufacturer(0x0118) // Radius Networks or any 2-byte Bluetooth SIG company code
-                .setTxPower(-59)
-                .build();
-        BeaconParser beaconParser = new BeaconParser()
-                .setBeaconLayout(BeaconParser.EDDYSTONE_EID_LAYOUT);
-        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
-        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
-
-            @Override
-            public void onStartFailure(int errorCode) {
-                Log.e(TAG, "Advertisement start failed with code: " + errorCode);
-            }
-
-            @Override
-            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
-                Log.i(TAG, "Advertisement start succeeded.");
-            }
-        });
-        if (DEBUG) Log.d(DEBUG_TAG, "Exiting advertise");
+        if (DEBUG) Log.d(TAG, "onPause");
     }
 }
